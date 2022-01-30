aadb7f4a4dd5fbcd3ae98f29d755b34daeac29aa
Sunaljabili <you@example.com>
2022-01-30 18:59:11 +0400

    tasks completed

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+// Task-1
+// function checkNums(num1, num2) {
+//     if(num1 === 100 || num2 === 100 || num1+num2 === 100){
+//         return true;
+//     }
+//     return false;
+// }
+// console.log(checkNums(100,30));
+
+//Task-2
+// function checkName (names) {
+//     if (names== names.toUpperCase() || names==names.toLowerCase()) {
+//         return true;
+//     }
+//     else{    
+//         return false;
+//     }
+// }
+
+// console.log(checkName("cavanshir"));
+
+// Task-3
+
+// function getCredit(month, money) {
+//   let part = money % month;
+//   let payment = (money - part) / month;
+//   for (let i = 1; i <= month; i++) {
+//     if (i <month) {
+//       console.log(`${i}-ci ay ${payment} azn odenish`);
+//     } else {
+//       console.log(`${i}-ci ay  ${payment + part} azn odenish `);
+//     }
+//   }
+// }
+
+// getCredit(3, 1000);
+
+
+//Task-4
+// let array = ["Ferhad" , "Baxtiyar" , "Erestun"]
+
+// function getSix (array){
+
+//     const result = array.filter(array => array.length > 6);
+//     return result ;
+// }
+// console.log (getSix(array))
+
+//Task-5
+
+// let names =["Hezret","Erestun","Dunya"];
+
+// function sortWords(names){
+//   return  names.sort().reverse();
+// }
+// console.log(sortWords(names));
+
+// let myFunc = (str) => {
+//     let result = str.slice(str.lastIndexOf("."))
+//     return result;
+// }
+
+// console.log(myFunc("index.html"));
+
+//Task-6
+
+// let num1 =7;
+// console.log(typeof num1);
+// let firstName ="Hezret";
+// console.log(typeof firstName);
+
+// let isMmarried=true;
+// log
+
+// console.log(1>2>3);
+
